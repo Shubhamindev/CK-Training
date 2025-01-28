69244af627191a3b4527ed6d7813ff7e73f0d576
Shubham Kumar <79997250+Shubhamindev@users.noreply.github.com>
2025-01-28 16:42:22 +0530

    Update es.js

--- day6/es.js
+++ day6/es.js
@@ -1,4 +1,14 @@
+
+//1
 let add=(a,b) => {return a+b}
 console.log(add(5,9))
+
+
+//2
+function greet(name, message = "Welcome!") {
+  return `${message}, ${name}!`;
+}
+console.log(greet("Shubham"));
+console.log(greet("Annu", "Hello")); 
 
 
