4476aea781f689dd7c4c0f4f3685862bd328dbb1
Shubham Kumar <79997250+Shubhamindev@users.noreply.github.com>
2025-01-28 17:54:04 +0530

    Update es.js

--- day6/es.js
+++ day6/es.js
@@ -28,4 +28,25 @@ const person = {
 
 const{name,city} = person;
 
-#5
+
+//5
+
+function sumAll(...numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0);
+}
+console.log(sumAll(1, 2, 3, 4));
+
+
+//6
+
+function filterEvens(numbers) {
+  return numbers.filter(num => num % 2 === 0);
+}
+console.log(filterEvens([2, 4, 8, 10])); 
+
+//7
+function doubleValues(numbers) {
+  return numbers.map(num => num * 2);
+}
+
+console.log(doubleValues([1,2,3,43,2]))
